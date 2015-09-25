d37458e987739cd1302de9ce10b431c036253277
Marujo <[joppk001@hotmail.com]>
2015-09-25 19:04:38 +0700

    update file

--- Array01.java
+++ Array01.java
@@ -1,11 +1,16 @@
-public class Array01 {
-  public static void main (String args[]) {
-      int[] a;
-      
-      // Assign Value to A
-      
-      // Print a Array
-      
-      // Print Array
+import java.util.Arrays;
+import java.util.Scanner;
+	public class array01 
+  {
+		public static void main (String args[]) {
+			Scanner in=new Scanner(System.in);
+		    System.out.print("Enter :");
+    	    int []a =new int[9];
+		    String s1 = in.next();
+				      
+    	    for (int i = 0; i < s1.length(); i++) {
+			a[i]=Character.getNumericValue((s1.charAt(i)));
+	   		System.out.print(Arrays.toString(a));
+	        }
   }
 }
